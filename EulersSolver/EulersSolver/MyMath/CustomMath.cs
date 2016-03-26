fc4051b3b404002f3b7ae9cb19ecd3a0c59a5493
wentimo <wentimo@gmail.com>
2016-03-26 12:32:35 -0400

    Updates to 543

--- EulersSolver/EulersSolver/MyMath/CustomMath.cs
+++ EulersSolver/EulersSolver/MyMath/CustomMath.cs
@@ -1,5 +1,6 @@
 ﻿using EulersSolver.Extensions;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -15,10 +16,22 @@ namespace EulersSolver.MyMath
             return i * bigFactorial(i - 1);
         }
 
-        public static List<int> GetFibonacciNumbers(int max)
+        public static List<int> GeneratetFibonacciNumbers(int count)
         {
-            var returnList = new List<int>();
+            if (count == 0) return null;
+            if (count == 1) return new List<int> { 0 };
+            if (count == 2) return new List<int> { 0, 1 };
 
+            int fibX = 1, fibY = 1, fibZ = 0;
+            var returnList = new List<int> { 0, 1, 1 };
+
+            while (returnList.Count <= count)
+            {
+                fibZ = fibX + fibY;
+                fibX = fibY;
+                fibY = fibZ;
+                returnList.Add(fibZ);
+            }
 
             return returnList;
         }
@@ -108,4 +121,128 @@ namespace EulersSolver.MyMath
             //return new List<int>(Enumerable.Range(1, max).Where(x => isPrime[x]));
         }
     }
+}
+
+namespace StackOverflow_Math
+{
+    // All credit goes to David Johnstone for these implementations listed here:
+    // https://stackoverflow.com/questions/1042902/most-elegant-way-to-generate-prime-numbers
+    public class OutsideMath
+    {
+        public static List<int> GeneratePrimesNaive(int n)
+        {
+            List<int> primes = new List<int>();
+            primes.Add(2);
+            int nextPrime = 3;
+            while (primes.Count < n)
+            {
+                int sqrt = (int)Math.Sqrt(nextPrime);
+                bool isPrime = true;
+                for (int i = 0; (int)primes[i] <= sqrt; i++)
+                {
+                    if (nextPrime % primes[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primes.Add(nextPrime);
+                }
+                nextPrime += 2;
+            }
+            return primes;
+        }
+
+        public static int ApproximateNthPrime(int nn)
+        {
+            double n = (double)nn;
+            double p;
+            if (nn >= 7022)
+            {
+                p = n * Math.Log(n) + n * (Math.Log(Math.Log(n)) - 0.9385);
+            }
+            else if (nn >= 6)
+            {
+                p = n * Math.Log(n) + n * Math.Log(Math.Log(n));
+            }
+            else if (nn > 0)
+            {
+                p = new int[] { 2, 3, 5, 7, 11 }[nn - 1];
+            }
+            else
+            {
+                p = 0;
+            }
+            return (int)p;
+        }
+
+        // Find all primes up to and including the limit
+        public static BitArray SieveOfEratosthenes(int limit)
+        {
+            BitArray bits = new BitArray(limit + 1, true);
+            bits[0] = false;
+            bits[1] = false;
+            for (int i = 0; i * i <= limit; i++)
+            {
+                if (bits[i])
+                {
+                    for (int j = i * i; j <= limit; j += i)
+                    {
+                        bits[j] = false;
+                    }
+                }
+            }
+            return bits;
+        }
+
+        public static List<int> GeneratePrimesSieveOfEratosthenes(int n)
+        {
+            int limit = ApproximateNthPrime(n);
+            BitArray bits = SieveOfEratosthenes(limit);
+            List<int> primes = new List<int>();
+            for (int i = 0, found = 0; i < limit && found < n; i++)
+            {
+                if (bits[i])
+                {
+                    primes.Add(i);
+                    found++;
+                }
+            }
+            return primes;
+        }
+
+        public static BitArray SieveOfSundaram(int limit)
+        {
+            limit /= 2;
+            BitArray bits = new BitArray(limit + 1, true);
+            for (int i = 1; 3 * i + 1 < limit; i++)
+            {
+                for (int j = 1; i + j + 2 * i * j <= limit; j++)
+                {
+                    bits[i + j + 2 * i * j] = false;
+                }
+            }
+            return bits;
+        }
+
+        public static List<int> GeneratePrimesSieveOfSundaram(int n)
+        {
+            int limit = ApproximateNthPrime(n);
+            BitArray bits = SieveOfSundaram(limit);
+            List<int> primes = new List<int>();
+            primes.Add(2);
+            for (int i = 1, found = 1; 2 * i + 1 <= limit && found < n; i++)
+            {
+                if (bits[i])
+                {
+                    primes.Add(2 * i + 1);
+                    found++;
+                }
+            }
+            return primes;
+        }
+    }
+
 }
--- EulersSolver/EulersSolver/Problems/100+/Problem543.cs
+++ EulersSolver/EulersSolver/Problems/100+/Problem543.cs
@@ -1,9 +1,10 @@
 ﻿using EulersSolver.Extensions;
+using StackOverflow_Math;
+using EulersSolver.MyMath;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
-using EulersSolver.MyMath;
 
 namespace EulersSolver.Problems
 {
@@ -26,41 +27,37 @@ namespace EulersSolver.Problems
 
             */
 
-            int x = 100;
+            //int x = 700000000;
+            //var primes = OutsideMath.SieveOfEratosthenes(x);
+            //int y = Enumerable.Range(1, x).Where(z => primes[z]).Count();
 
-            var primes = CustomMath.GetListOfPrimes(x);
-            DebugLogger.AddLine(primes.Count);
+            //int lowEnd = 3;
+            //int highEnd = 44;
 
-            var dprimeCount = x/Math.Log(x);
-            DebugLogger.AddLine(dprimeCount);
+            //var fib = CustomMath.GeneratetFibonacciNumbers(highEnd);
+            //fib = fib.Skip(lowEnd).Take(fib.Count() - lowEnd).ToList();
 
-            var y = S_new(100);
-            return y.ToString();
-        }
-        
-        private BigInteger F(int k)
-        {
-            if (k == 0) return 0;
-            if (k < 3) return 1;
+            //foreach (var prime in fib)
+            //{
+            //    DebugLogger.AddLine($"F({lowEnd++}) = {prime}");
+            //}
 
-            int fibX = 0, fibY = 1, fibZ = 0, count = 2;
-            BigInteger sum = 0;
-            var x = new List<Tuple<int, int>>();
+            //var y = S_new(39088169);
 
-            while (count <= k)
+            //Enumerable.Range(2, 19).ForEach(x => DebugLogger.AddLine($"S({x}) = {S(x)}"));
+            var prevValue = S(1);
+            foreach (var value in Enumerable.Range(2, 38))
             {
-                fibZ = fibX + fibY;
-                fibX = fibY;
-                fibY = fibZ;
-
-                if (count > 2) sum += S(fibZ, ref x);
-                count++;
+                
+                string message = $"S({value}) = {S(value)}";
+              //  DebugLogger.AddLine(message);
             }
-
-            return sum;
+            //var y = S(100);
+            // return y.ToString();
+            return 1.ToString();
         }
-
-        private BigInteger S_new(int n)
+     
+        private BigInteger S(int n)
         {
             BigInteger sum = 0;
 
@@ -69,158 +66,24 @@ namespace EulersSolver.Problems
             {
                 sum += i / 2 - 2;
             }            
-            DebugLogger.AddLine($"j >= 3 logic : 0 (+{sum})");
+            DebugLogger.AddLine($"S({n, 2}) = 0 (+{sum})");
             var diff = sum;
 
             sum += n / 2;
-            DebugLogger.AddLine($"j == 2 && i % 2 == 0 logic : {sum} (+{sum - diff})");
+            //DebugLogger.AddLine($"j == 2 && i % 2 == 0 logic : {sum} (+{sum - diff})");
             diff = sum;
 
             var primes = CustomMath.GetListOfPrimes(n);
+           // var primes = OutsideMath.SieveOfEratosthenes(n);
             int primeCount = primes.Count;
-
-            //var dprimeCount = n/Math.Log(n);
-            //var primeCount = (int)dprimeCount;
-
+            
             sum += primeCount - 2;
-            DebugLogger.AddLine($"j == 2 && i % 2 == 1 logic : {sum} (+{sum - diff})");
+            //DebugLogger.AddLine($"j == 2 && i % 2 == 1 logic : {sum} (+{sum - diff})");
             diff = sum;
 
             sum += primeCount;
-            DebugLogger.AddLine($"j == 1 logic : {sum} (+{sum - diff})");
+            //DebugLogger.AddLine($"j == 1 logic : {sum} (+{sum - diff})");
             return sum;
-        }
-
-        private BigInteger S(int n, ref List<Tuple<int, int>> TupleList)
-        {
-            BigInteger sum = 0;
-
-            for (int i = n; i > 0; i--)
-            {
-                for (int j = i / 2; j > 1; j--)
-                {
-                    if (j == 2)
-                    {
-                        if (i % 2 == 0 || isPrime(i - 2))
-                        {
-                            sum++;
-                            //DebugLogger.AddLine($"P({i,3}, {j,3}) = 1");
-                            TupleList.Add(new Tuple<int, int>(i, j));
-                        }
-                    }
-                    else
-                    {
-                        sum++;
-                        //DebugLogger.AddLine($"P({i,3}, {j,3}) = 1");
-                        TupleList.Add(new Tuple<int, int>(i, j));
-                    }
-                }
-
-                if (isPrime(i))
-                {
-                    sum++;
-                    //DebugLogger.AddLine($"P({i, 3}, {1, 3}) = 1");
-                    TupleList.Add(new Tuple<int, int>(i, 1));
-                }
-            }
-            return sum;
-        }
-
-        private BigInteger S_old(int n, ref List<Tuple<int, int>> TupleList)
-        {
-            //Let S(n)be the sum of all P(i, k) over 1 ≤ i, k ≤ n.
-            //For example, S(10) = 20, S(100) = 2402, and S(1000) = 248838.
-            BigInteger sum = 0;
-
-            for (int i = 1; i <= n; i++)
-            {
-                bool started = false;
-
-                for (int j = 1; j <= n; j++)
-                {
-                    int incr = P(i, j);
-                    if (incr > 0 && j > 4) started = true;
-                    //if (incr > 0)
-                    //{
-                    //    DebugLogger.AddLine($"P({i,-3},{j,-3}) = {incr}, Sum = {sum + incr} (+{incr})");
-                    //}
-                    //sum += incr;
-                    if (incr > 0)
-                    {
-                        sum++;
-                        TupleList.Add(new Tuple<int, int>(i, j));
-                    }
-                    if (started && incr == 0) break;
-                }
-            }
-            return sum;
-        }
-
-        private int P(int n, int t)
-        {
-            //  Define function P(n, k) = 1 if n can be written as the sum of k prime numbers(with repetitions allowed), and P(n, k) = 0 otherwise.
-            //  For example, P(10, 2) = 1 because 10 can be written as either 3 + 7 or 5 + 5, but P(11, 2) = 0 because no two primes can sum to 11.
-
-            //   var line = $"P({n,3},{t,3}) | n/t = {(double)n / (double)t, 16}, t/n = {(double)t / (double)n, 8}";
-            if (t / n > 0.5 || t / n < 0.0) return 0;
-
-            if (n % 2 == 0)
-            {
-                if (t == 1 && n == 2)
-                {
-                    //DebugLogger.AddLine(line);
-                    return 1;
-                }
-                if (t == 2 && n > 2)
-                {
-                    //DebugLogger.AddLine(line);
-                    return 1;
-                }
-                if (t > 2 && ((t * 2) <= n || t == 2))
-                {
-                    //DebugLogger.AddLine(line);
-                    return 1;
-                }
-                return 0;
-            }
-            else
-            {
-                if (t == 1 && isPrime(n))
-                {
-                    //DebugLogger.AddLine(line);
-                    return 1;
-                }
-                if (t == 2)
-                {
-                    if (n <= 3) return 0;
-                    if (isPrime(n - 2))
-                    {
-                        //DebugLogger.AddLine(line);
-                        return 1;
-                    }
-                    return 0;
-                }
-                if (t >= 3 && (t * 2) <= n)
-                {
-                    //DebugLogger.AddLine(line);
-                    return 1;
-                }
-                return 0;
-            }
-        }
-
-        public static bool isPrime(int number)
-        {
-            var boundary = number.Sqrt();
-            if (number < 2) return false;
-            if (number == 2) return true;
-
-            for (int i = 2; i <= boundary; ++i)
-            {
-                if (number % i == 0) return false;
-            }
-
-            return true;
         }
     }
 }
